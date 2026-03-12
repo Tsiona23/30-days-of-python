61fd3cd049cc554a1dc45c626fa6837054cab3ce
Tsiona23 <tsionawithaile@gmail.com>
2026-03-12 21:10:45 +0300

    added exercise.py

---
+++ Day9_loops/exercises.py
@@ -0,0 +1,16 @@
+#  write a program that prints numbers from 1-10
+for i in range(1, 11):
+    print(i)
+
+# print even numbers from 1 to 20
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+
+# write a program that prints sum of numbers from 1-10
+sum = 0
+for i in range(1, 11):
+     sum = sum + i
+     print( "sum =", sum)     
+
+ 
