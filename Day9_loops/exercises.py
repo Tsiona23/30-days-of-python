3bb2e6906c38d8021de2001ab1c8d5613be8b1ab
Tsiona23 <tsionawithaile@gmail.com>
2026-03-12 21:17:58 +0300

    and another program to the exercise.py

--- Day9_loops/exercises.py
+++ Day9_loops/exercises.py
@@ -12,5 +12,9 @@ sum = 0
 for i in range(1, 11):
      sum = sum + i
      print( "sum =", sum)     
-
- 
+# write a program that asks the user to enter a word and count how many characters it has using a loop.
+word = input("Enter a word: ")
+count = 0
+for char in word:
+    count += 1
+print("Number of characters:", count)
