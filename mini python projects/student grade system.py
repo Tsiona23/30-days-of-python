985783c1c67dcbb7fe2fd4f3c859b36e4004dbec
Tsiona23 <tsionawithaile@gmail.com>
2026-03-17 15:56:53 +0300

    added a mini project

---
+++ mini python projects/student grade system.py
@@ -0,0 +1,11 @@
+#Create a system that:Takes student marks,Calculates average, and Assigns grade.
+def count_vowels(text):
+    vowels = "aeiouAEIOU"
+    count = 0
+    
+    for char in text:
+        if char in vowels:
+         count += 1
+    return count
+
+print(count_vowels("Python"))
