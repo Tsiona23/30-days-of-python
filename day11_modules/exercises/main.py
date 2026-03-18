0cd321bebaad110b968d123f509d3e89a247c626
Tsiona23 <tsionawithaile@gmail.com>
2026-03-18 10:35:03 +0300

    added exercises

---
+++ day11_modules/exercises/main.py
@@ -0,0 +1,8 @@
+import math_utils
+import string_utils
+
+print(math_utils.add(5, 3))
+print(math_utils.multiply(2, 4))
+
+print(string_utils.to_upper("hello"))
+print(string_utils.count_chars("python"))
---
+++ day11_modules/exercises/string_utils.py
@@ -0,0 +1,5 @@
+def to_upper(text):
+    return text.upper()
+
+def count_chars(text):
+    return len(text)
