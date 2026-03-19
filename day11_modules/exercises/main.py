c8ddfcfa63ecedfbb7451691f729778c80a607c8
Tsiona23 <tsionawithaile@gmail.com>
2026-03-19 18:58:41 +0300

    updated the file

--- day11_modules/exercises/main.py
+++ day11_modules/exercises/main.py
@@ -5,4 +5,4 @@ print(math_utils.add(5, 3))
 print(math_utils.multiply(2, 4))
 
 print(string_utils.to_upper("hello"))
-print(string_utils.count_chars("python"))
+print(string_utils.count_chars("python")) 
